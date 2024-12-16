af3447528ba0833e92a3260eb665f32b28ac6e0f
n-szilard <nagyapatiszilard@turr.hu>
2024-12-16 08:42:48 +0100

    SCRUM-27 Főoldal asztali nézet reklám kártyák kész

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,26 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    colors: {
-      "tarsas1" : {
+    extend: {
+      colors: {
+        "tarsas1": {
           light: "#F5EBEB",
           dark: "#DBAFA0"
-      },
-      "tarsas2" : {
-        light: "#E4D0D0",
-        dark: "#BB8493"
-      },
-      "tarsas3" : {
-        light: "#D5B4B4",
-        dark: "#704264"
-      },
-      "tarsas4" : {
-        light: "#867070",
-        dark: "#49243E"
-      },
+        },
+        "tarsas2": {
+          light: "#E4D0D0",
+          dark: "#BB8493"
+        },
+        "tarsas3": {
+          light: "#D5B4B4",
+          dark: "#704264"
+        },
+        "tarsas4": {
+          light: "#867070",
+          dark: "#49243E"
+        }
+      }
     }
-  },
-  plugins: [],
+  }
 }
 
